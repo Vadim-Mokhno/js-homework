227488e6ef2a7c59c9bece73f2c5408886c7eb2e
Vadim-Mokhno <92451101+Vadim-Mokhno@users.noreply.github.com>
2023-06-18 17:15:27 +0000

    refactor task 6 in homework 2

--- 2-operators-types-arrays/task-6.js
+++ 2-operators-types-arrays/task-6.js
@@ -1,11 +1,15 @@
 // Task 6
 let arr = [4, 5, 2, 1, 6, 5, 3, 5, 2, 5];
+console.log('initial array: [' + arr + ']');
 
 const mostFrequentNumberInArray = arr
   .map((element, index, array) => [
     element,
-    array.filter(el => el === element).length,
+    array.indexOf(element) < index
+      ? -1
+      : array.filter(el => el === element).length,
   ])
+  .filter(el => el[1] !== -1)
   .reduce((mostFrequentNumberInArrayEntry, entry) => {
     return entry[1] > mostFrequentNumberInArrayEntry[1]
       ? entry
@@ -16,5 +20,5 @@ const mostFrequentNumberInArray = arr
 arr = arr.filter(el => el !== mostFrequentNumberInArray);
 const data = [mostFrequentNumberInArray];
 
-console.log(arr);
-console.log(data);
+console.log('array with the most frequent number in the initial array: [' + data + ']');
+console.log('array after deletion of the most frequent number: [' + arr + ']');
