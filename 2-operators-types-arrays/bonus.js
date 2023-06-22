b3e81cd7e26dc14325e565a10c90b18d680b57e5
Vadim-Mokhno <92451101+Vadim-Mokhno@users.noreply.github.com>
2023-06-22 20:51:25 +0000

    fix bonus task in homework 2

--- 2-operators-types-arrays/bonus.js
+++ 2-operators-types-arrays/bonus.js
@@ -3,44 +3,63 @@ const revenueRateForFixedNumberOfLines = 50;
 const fixedNumberOfLines = 100;
 const fineRate = 20;
 const fineFrequency = 3;
+let desiredIncome;
+let numberOfLines;
+let numberOfLatenesses;
+let salary;
 
-let desiredIncome = Number(prompt('Enter your desired income:'));
-const numberOfLatenesses = Number(
-  prompt('Enter the number of your latenesses:')
-);
+const userChoice = Number(prompt(`What do you want to compute? :
+1 - number of code lines
+2 - number of latenesses
+3 - salary value`));
 
-alert(
-  `You must write ${
-    Math.ceil(
-      ((desiredIncome +
-        Math.floor(numberOfLatenesses / fineFrequency) * fineRate) *
-        fixedNumberOfLines) /
-        revenueRateForFixedNumberOfLines /
-        fixedNumberOfLines
-    ) * fixedNumberOfLines
-  } lines of code`
-);
 
-const numberOfLines = Number(
-  prompt('Enter the number of code lines you wrote:')
-);
-desiredIncome = Number(prompt('Enter your desired income:'));
+switch (userChoice) {
+  case 1:
+    desiredIncome = Number(prompt('Enter your desired income:'));
+    numberOfLatenesses = Number(
+      prompt('Enter the number of your latenesses:')
+    );
 
-alert(
-  `You can be late ${Math.floor(
-    ((Math.floor(numberOfLines / fixedNumberOfLines) *
-      revenueRateForFixedNumberOfLines -
-      desiredIncome) /
-      fineRate) *
-      fineFrequency
-  )} times`
-);
+    alert(
+      `You must write ${
+        Math.ceil(
+          ((desiredIncome +
+            Math.floor(numberOfLatenesses / fineFrequency) * fineRate) /
+            (revenueRateForFixedNumberOfLines / fixedNumberOfLines)) /
+            fixedNumberOfLines
+        ) * fixedNumberOfLines
+      } lines of code`
+    );
+    break;
 
-numberOfLines = Number(prompt('Enter the number of code lines you wrote:'));
-numberOfLatenesses = Number(prompt('Enter the number of your latenesses:'));
-const salary =
-  Math.floor(numberOfLines / fixedNumberOfLines) *
-    revenueRateForFixedNumberOfLines -
-  Math.floor(numberOfLatenesses / fineFrequency) * fineRate;
+  case 2:
+    numberOfLines = Number(
+      prompt('Enter the number of code lines you wrote:')
+    );
+    desiredIncome = Number(prompt('Enter your desired income:'));
+    numberOfLatenesses = Math.floor(
+        ((Math.floor(numberOfLines / fixedNumberOfLines) *
+          revenueRateForFixedNumberOfLines -
+          desiredIncome) /
+          (fineRate / fineFrequency))           
+    );
+    alert(
+      `You can be late ${
+        numberOfLatenesses +
+        (fineFrequency - (numberOfLatenesses % fineFrequency) - 1)
+      } times`
+    );
+    break;
+  case 3:
+    numberOfLines = Number(prompt('Enter the number of code lines you wrote:'));
+    numberOfLatenesses = Number(prompt('Enter the number of your latenesses:'));
+    salary =
+      Math.floor(numberOfLines / fixedNumberOfLines) *
+        revenueRateForFixedNumberOfLines -
+      Math.floor(numberOfLatenesses / fineFrequency) * fineRate;
 
-alert(`You earned ${salary > 0 ? salary + '$' : 'nothing'}`);
+    alert(`You earned ${salary > 0 ? salary + '$' : 'nothing'}`);
+  default:
+    alert('Incorect input!');
+}
