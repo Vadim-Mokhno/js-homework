7818fc7280af66329974c7b1416b20b89a9ca1b1
Vadym Mokhno <vadim1mokhno@gmail.com>
2023-06-29 20:51:11 +0300

    fix 1 and 4 tasks in homework-4

--- task-1.js
+++ task-1.js
@@ -7,25 +7,27 @@ const sumSliceArray = function (array, first, second) {
     throw new TypeError('indexes in array must numbers');
   }
 
-  if (first < 0 || first >= array.length) {
+  if (first < 1 || first > array.length) {
     throw new RangeError('first index is out of range');
   }
 
-  if (second < 0 || second >= array.length) {
+  if (second < 1 || second > array.length) {
     throw new RangeError('second index is out of range');
   }
 
-  return array[first] + array[second];
+  return array[first - 1] + array[second - 1];
 };
 
 try {
-  const array = [2, 4, null, 65, 78, 34, 4, 56, 55, true, 77, undefined, {}];
-  const firstElementIndex = 4;
-  const secondElementIndex = -2;
+  const array = [2, 4, 22, 65, 78, 34, 4, 56, 55, true, 77, 55, 100];
+  const firstElement = 3;
+  const secondElement = 5;
   console.log(
-    `${array[firstElementIndex]} + ${
-      array[secondElementIndex]
-    } = ${sumSliceArray(array, firstElementIndex, secondElementIndex)}`
+    `${array[firstElement - 1]} + ${array[secondElement - 1]} = ${sumSliceArray(
+      array,
+      firstElement,
+      secondElement
+    )}`
   );
 } catch (e) {
   console.error(e.name);
