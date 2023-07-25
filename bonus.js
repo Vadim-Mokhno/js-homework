41d51c646ad697a9f548b3c4330261a87ba2182a
Vadym Mokhno <vadim1mokhno@gmail.com>
2023-07-25 17:44:50 +0300

    add Promises version for bonus task -n homework-11

--- bonus.js
+++ bonus.js
@@ -2,10 +2,26 @@ const delayLimit = 3000;
 const delay = (i, time) =>
   new Promise(resolve => setTimeout(() => resolve(i), time));
 
-async function showNumbers() {
+// Async/await version
+async function showNumbersAs() {
   for (let i = 0; i < 10; i++) {
     console.log(await delay(i, Math.floor(Math.random() * (delayLimit + 1))));
   }
 }
 
-showNumbers();
+// Pomises version
+async function showNumbersPr() {
+  let promiseDelay = 0;
+  let time;
+  for (let i = 0; i < 10; i++) {
+    time = Math.floor(Math.random() * (delayLimit + 1));
+    setTimeout(
+      () => delay(i, time).then(res => console.log(res)),
+      promiseDelay
+    );
+    promiseDelay += time;
+  }
+}
+
+showNumbersAs();
+// showNumbersPr();
